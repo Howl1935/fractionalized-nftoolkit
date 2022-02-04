db609920e844bd437382569abf63ae1f910c36e1
Howl1935 <travisphawley@gmail.com>
2022-02-04 10:50:04 -0800

    a little messy right now but ready to pull other info

---
+++ frontend/src/features/covalent/covalentService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+const API_URL = 'https://api.covalenthq.com/v1/'
+
+
+//1/address/0xDec7778a7E416b0f4988Bb1Faff70cE9FAD6C233/balances_v2/?no-nft-fetch=false&key=ckey_1e19f00677f24bd4a7e0479f7d0&nft=true
+// Register user
+const getNFTsETH = async (walletAddress) => {
+    const response = await axios.get(`${API_URL}1/address/${walletAddress}/balances_v2/?no-nft-fetch=false&key=${process.env.REACT_APP_COVALENT_API_KEY}&nft=true`)
+    return response.data.data
+  }
+  
+  
+
+
+const covalentService = {
+    getNFTsETH,
+
+}
+export default covalentService
--- frontend/src/pages/CovalentTest.jsx
+++ frontend/src/pages/CovalentTest.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import {getNFTsETH , getNfts} from '../features/covalent/covalentSlice'
+import Spinner from '../components/layout/spinner/Spinner';
+import NftResult from '../components/nfts/NftResult';
+
 
 function CovalentTest() {
-  console.log(process.env.REACT_APP_COVALENT_API_KEY);
-  return <div>HIIII</div>;
+  const {nfts, isLoading, isSuccess} = useSelector(state => state.covalent);
+
+  const dispatch = useDispatch();
+
+  const onSubmit=(e)=>{
+    e.preventDefault();
+    dispatch(getNFTsETH('0xDec7778a7E416b0f4988Bb1Faff70cE9FAD6C233'))
+  }
+
+  useEffect(()=>{
+    if(isSuccess){
+      dispatch(getNfts())
+    }
+    if(nfts !== null){
+      console.log('with great success')
+    }
+  },[isSuccess, nfts, dispatch])
+
+  if(isLoading){
+    return <Spinner />
+  }
+
+  return <div>
+    <button onClick={onSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">CLICK ME</button>
+    {nfts !== null && (<NftResult />)}
+  </div>;
 }
 
 export default CovalentTest;

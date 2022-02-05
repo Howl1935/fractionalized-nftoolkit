f6106d19f60c2a019206fa948b08784f58a3d8e1
maxbettinelli <73137949+maxbettinelli@users.noreply.github.com>
2022-02-04 19:23:12 -0800

    Changed the button

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -4,7 +4,7 @@ import ConnectWallet from "../ConnectWallet";
 import PropTypes from "prop-types";
 function Navbar({ title }) {
   return (
-    <nav class="flex items-center border rounded justify-between flex-wrap bg-lime-300 p-6">
+    <nav class="flex items-center border roundedjustify-between flex-wrap bg-lime-300 p-6">
   <div class="flex items-center flex-shrink-0 text-black mr-6">
     <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
     <span class="font-semibold text-xl tracking-tight">Etherreal</span>
@@ -17,17 +17,17 @@ function Navbar({ title }) {
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
       <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 hover:text-white mr-4">
-        Docs
+        Max
       </a>
       <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 hover:text-white mr-4">
-        Examples
+        Matt
       </a>
       <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 hover:text-white">
-        Blog
+        Travis
       </a>
     </div>
     <div>
-      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-bg-lime-600 border-bg-lime-600 hover:border-black hover:text-black hover:bg-bg-lime-500 mt-4 lg:mt-0">
+      <a class="inline-block text-sm px-4 py-2 leading-none bg-lime-500 border rounded text-black hover:border-black hover:text-black hover:bg-lime-500 mt-4 lg:mt-0">
 		  <ConnectWallet />
 	  </a>
     </div>

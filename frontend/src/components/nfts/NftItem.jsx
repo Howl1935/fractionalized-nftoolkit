9574673d8034e041f0008149040ea628d42d1035
Matt <mattdepauw@gmail.com>
2022-02-05 20:56:17 -0800

    started adding boxes w/ text below NFT item

--- frontend/src/components/nfts/NftItem.jsx
+++ frontend/src/components/nfts/NftItem.jsx
@@ -1,11 +1,28 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 function NftItem({ nft: { target_id, image, name } }) {
-	return (
-		<div className='bg-yellow-500 p-5 rounded-md'>
-			<h1 className="text-black text-lg ">{name}</h1>
-			<img className='rounded-md' src={image} alt='Profile' />
-			{/* <h1 className="text-black">{contract_ticker_symbol}</h1> */}
-		</div>
-	);
+	let currentPrice = 30000;
+	let purchasePrice = 10000;
+	let profit = currentPrice - purchasePrice;
+
+	let title = "#314 Leo's Dream"
+  return (
+    <div class="flex flex-col space-y-3 ">
+      <div className="bg-yellow-500 p-5 rounded-md">
+        <h1 className="text-black text-lg ">{name}</h1>
+        <img className="rounded-md" src={image} alt="Profile" />
+        {/* <h1 className="text-black">{contract_ticker_symbol}</h1> */}
+      </div>
+	  <div class = "shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#E4EDF4] rounded-[15px] min-12">	
+			<div class="flex flex-col">
+				<h1 class= "text-center text-3xl">{title}</h1>
+				<div class="flex flex-row ml-2">
+					<p>Current price: ${currentPrice} </p>
+					<p>Purchase price: ${purchasePrice} </p>
+					<p>Profit: ${profit}</p>
+				</div>
+			</div>
+	  </div>
+    </div>
+  );
 }
 export default NftItem;
--- frontend/src/components/nfts/ethbanner/Ethbanner.jsx
+++ frontend/src/components/nfts/ethbanner/Ethbanner.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ethLogo from "../../assets/eth-logo.png";
 
 function Ethbanner() {
+  let network = "ETHEREUM"
+  let nftCount = 51;
   return (
     <div>
       <div class="flex flex-row space-x--1">
-        <p class="text-5xl">ETHEREUM</p>
+        <p class="text-5xl">{network}</p>
         <img
           class="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] -mx-5 -my-1.5"
           src={ethLogo}
@@ -16,7 +18,7 @@ function Ethbanner() {
       </div>
       <div class="flex flex-row space-x-1 -mt-4">
         <p class="text-lg">total NFTS:</p>
-        <p class="text-lg text-[#FFC702] ">51</p>
+        <p class="text-lg text-[#FFC702] ">{nftCount}</p>
       </div>
     </div>
   );

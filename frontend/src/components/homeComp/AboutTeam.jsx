e6007580fd49c20afd39d98fa6d2f3978f7ca3a7
Matt <mattdepauw@gmail.com>
2022-02-06 14:28:38 -0800

    created 'About Team', added images needed, links to socials

---
+++ frontend/src/components/homeComp/AboutTeam.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import cpLogo from "../assets/CalPolyLogo.png";
+import cpBlock from "../assets/cpblockchain_1.png";
+import githubLogo from "../assets/githubLogo.png";
+import linkedinLogo from "../assets/linkedin-icon.png";
+
+function AboutTeam() {
+  return (
+    <div className="flex flex-row space-x-48  bg-[#6EBD84] shadow-[0_4px_4px_rgba(0,0,0,0.25)] w-[1342px] h-[279px]
+    rounded-[20px]">
+      <img className="scale-75 -ml-3" src={cpLogo}></img>
+      <div>
+        <div className="flex flex-col space-y-8 w-[18rem] ">
+          <h1 className="font-poppins text-[28px] font-bold text-center mt-2 text-[#575757]">
+            The Flourish Team:
+          </h1>
+          <div className="flex flex-col space-y-0.5">
+            <div className="flow-root">
+              <h1 className="font-poppins text-[28px] font-bold float-left text-[#FFFFFF]">
+                Max Bettinelli
+              </h1>
+             <div className="flex flex-row float-right space-x-1">
+             <a href="https://github.com/maxbettinelli"><img className="" src={githubLogo} width="42" height="33"></img></a>
+                <a href="https://github.com/maxbettinelli"><img className="mt-1" src={linkedinLogo} width="35" height="37"></img></a>
+                </div>
+            </div>
+
+            <div className="flow-root">
+              <h1 className="font-poppins text-[28px] font-bold float-left text-[#FFFFFF]">
+                Matt DePauw
+              </h1>
+              <div className="flex flex-row float-right space-x-1">
+              <a href="https://github.com/mattriots"><img className="" src={githubLogo} width="42" height="33"></img></a>
+                <a href="https://www.linkedin.com/in/matt-depauw/"><img className="mt-1" src={linkedinLogo} width="35" height="37"></img></a>
+                </div>
+            </div>
+
+            <div className="flow-root">
+              <h1 className="font-poppins text-[28px] font-bold float-left text-[#FFFFFF]">
+              Travis Hawley
+              </h1>
+              <div className="flex flex-row float-right space-x-1">
+                  <a href="https://github.com/Howl1935"><img className="" src={githubLogo} width="42" height="33"></img></a>
+                <a href="https://www.linkedin.com/in/travis-hawley-150a5420a/"><img className="mt-1" src={linkedinLogo} width="35" height="37" ></img></a>
+                
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        <a href="https://www.calpolyblockchain.com/">
+        <img className="mt-5" src={cpBlock}></img>
+        </a>
+     
+    </div>
+  );
+}
+export default AboutTeam;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,59 +1,64 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { getNFTsETH, getNFTsPOLY, getNfts, getNftsPOLY } from '../features/covalent/covalentSlice';
-import Spinner from '../components/layout/spinner/Spinner';
-import { useNavigate } from 'react-router-dom';
-import { useMetaMask } from 'metamask-react';
-import Metamask from '../components/Metamask';
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import {
+  getNFTsETH,
+  getNFTsPOLY,
+  getNfts,
+  getNftsPOLY,
+} from "../features/covalent/covalentSlice";
+import Spinner from "../components/layout/spinner/Spinner";
+import { useNavigate } from "react-router-dom";
+import { useMetaMask } from "metamask-react";
+import Metamask from "../components/Metamask";
+import AboutTeam from "../components/homeComp/AboutTeam";
 function Home() {
-	const { nfts, nftsPOLY, address, isLoading, isSuccess } = useSelector(
-		(state) => state.covalent
-	);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const { status } = useMetaMask();
+  const { nfts, nftsPOLY, address, isLoading, isSuccess } = useSelector(
+    (state) => state.covalent
+  );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { status } = useMetaMask();
 
-	useEffect(() => {
-		if (isSuccess) {
-			dispatch(getNfts());
-			dispatch(getNftsPOLY())
-		}
-		if (address !== null) {
-			dispatch(getNFTsETH(address));
-			dispatch(getNFTsPOLY(address));
-		}
-		if(nfts !== null){
-			navigate('/gallery')
-		}
-	}, [isSuccess, address, nfts, nftsPOLY, navigate]);
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(getNfts());
+      dispatch(getNftsPOLY());
+    }
+    if (address !== null) {
+      dispatch(getNFTsETH(address));
+      dispatch(getNFTsPOLY(address));
+    }
+    if (nfts !== null) {
+      navigate("/gallery");
+    }
+  }, [isSuccess, address, nfts, nftsPOLY, navigate]);
 
-	if (isLoading) {
-		return <Spinner />;
-	}
+  if (isLoading) {
+    return <Spinner />;
+  }
 
+  return (
+    <>
+      <AboutTeam />
+      <div >
+	 
+        {" "}
+        <div class="container mx-auto">
+          <div class="flex flex-row flex-wrap py-4">
+            <aside class="w-full sm:w-1/3 md:w-1/4 px-2">
+              <div class="sticky top-0 p-4 w-full">
+                <ul class="flex flex-col overflow-hidden"></ul>
+              </div>
+            </aside>
+            <main role="main" class="w-full sm:w-2/3 md:w-3/4 pt-1 px-2"></main>
+          </div>
+        </div>
+      </div>
+      {/* {nfts !== null && navigate('/gallery')} */}
 
-	return <>
-
-	<div> <div class="container mx-auto">
-    <div class="flex flex-row flex-wrap py-4">
-        <aside class="w-full sm:w-1/3 md:w-1/4 px-2">
-            <div class="sticky top-0 p-4 w-full">
-                <ul class="flex flex-col overflow-hidden">
-                    
-                </ul>
-            </div>
-        </aside>
-        <main role="main" class="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
-	
-
-        </main>
-    </div>
-</div>
-		</div>
-	{/* {nfts !== null && navigate('/gallery')} */}
-
-	{status === 'unavailable' && <Metamask />}
-	</>;
+      {status === "unavailable" && <Metamask />}
+    </>
+  );
 }
 
 export default Home;

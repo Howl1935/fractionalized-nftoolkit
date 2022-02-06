cb7b80e8464f763c1ec7cbe2b5e2dfd5ee1f0a77
maxbettinelli <73137949+maxbettinelli@users.noreply.github.com>
2022-02-05 23:13:22 -0800

    Added same size pictures

--- frontend/src/components/nfts/NftItem.jsx
+++ frontend/src/components/nfts/NftItem.jsx
@@ -6,13 +6,15 @@ function NftItem({ nft: { target_id, image, name } }) {
 
 	let title = "#314 Leo's Dream"
   return (
-    <div class="flex flex-col space-y-3 ">
+    <div class="flex flex-col space-y-3 box-border h-70">
       <div className="bg-yellow-500 p-5 rounded-md">
-        <h1 className="text-black text-lg ">{name}</h1>
-        <img className="rounded-md" src={image} alt="Profile" />
+        <h1 className="text-black text-lg place-content-center text-center">{name}</h1>
+        
+			<img className="mx-auto rounded-md min-h-52 max-h-52 " src={image} alt="Profile" />
+			
         {/* <h1 className="text-black">{contract_ticker_symbol}</h1> */}
       </div>
-	  <div class = "shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#E4EDF4] rounded-[15px] min-12">	
+	  <div class = "shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#ffffe0] rounded-[15px] min-12">	
 			<div class="flex flex-col">
 				<h1 class= "text-center text-3xl">{title}</h1>
 				<div class="flex flex-row ml-2">

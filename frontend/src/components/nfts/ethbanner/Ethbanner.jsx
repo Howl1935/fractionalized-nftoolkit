f691677501d4edef58daa638c73ab37dfada4c84
maxbettinelli <73137949+maxbettinelli@users.noreply.github.com>
2022-02-05 19:46:38 -0800

    Added spacing for ethereum logo

--- frontend/src/components/nfts/ethbanner/Ethbanner.jsx
+++ frontend/src/components/nfts/ethbanner/Ethbanner.jsx
@@ -3,20 +3,20 @@ import ethLogo from "../../assets/eth-logo.png";
 
 function Ethbanner() {
   return (
-    <div>
-      <div class="flex flex-row space-x--1">
-        <p class="text-5xl">ETHEREUM</p>
+    <div className="space-y-1">
+      <div className="flex flex-row space-x--1 ">
+        <p className="text-5xl">ETHEREUM</p>
         <img
-          class="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] -mx-5 -my-1.5"
+          className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] -mx-5 -my-1.5"
           src={ethLogo}
           alt="eth logo"
-          width="115"
-          height="115"
+          width="100"
+          height="100"
         />
       </div>
-      <div class="flex flex-row space-x-1 -mt-4">
-        <p class="text-lg">total NFTS:</p>
-        <p class="text-lg text-[#FFC702] ">51</p>
+      <div className="flex flex-row space-x-1 mt-4">
+        <p className="text-lg">Total NFTS:</p>
+        <p className="text-lg text-[#FFC702] ">51</p>
       </div>
     </div>
   );

06f67e87b4a0f5c2fc4db002408a806ccd937e4e
Howl1935 <travisphawley@gmail.com>
2022-02-06 13:26:28 -0800

    small layout changes

--- frontend/src/components/Intropage.jsx
+++ frontend/src/components/Intropage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 function Intropage({icon, text}) {
   return <div>
-      <div className="box-border rounded h-64 w-96 p-4 border-4 border-lime-400 hover:border-lime-230 text-center mt-40 bg-figma-green hover:bg-green-200">
+      <div className="box-border rounded h-64 w-96 p-4 border-4 border-lime-400 hover:border-lime-230 text-center mt-10 bg-figma-green hover:bg-green-200">
           <div className ="justify-center flex items-center w-full mt-10 mb-10">
              {icon} 
           </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,38 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import {
-  getNFTsETH,
-  getNFTsPOLY,
-  getNfts,
-  getNftsPOLY,
-} from "../features/covalent/covalentSlice";
-import Spinner from "../components/layout/spinner/Spinner";
-import { useNavigate } from "react-router-dom";
-import Metamask from "../components/Metamask";
-import Intropage from "../components/Intropage";
-import { FaBinoculars } from "react-icons/fa";
-import { FaChartLine } from "react-icons/fa";
-import { FaLinode } from "react-icons/fa";
-import TechStack from "../components/TechStack";
+	getNFTsETH,
+	getNFTsPOLY,
+	getNfts,
+	getNftsPOLY,
+} from '../features/covalent/covalentSlice';
+import Spinner from '../components/layout/spinner/Spinner';
+import { useNavigate } from 'react-router-dom';
+import Metamask from '../components/Metamask';
+import Intropage from '../components/Intropage';
+import { FaBinoculars } from 'react-icons/fa';
+import { FaChartLine } from 'react-icons/fa';
+import { FaLinode } from 'react-icons/fa';
+import TechStack from '../components/TechStack';
 import NavigationButtons from '../components/navigation/NavigationButtons';
 
-             
 function Home() {
-  const { nfts, nftsPOLY, userDataPOLY, isLoadingP, address, isLoading, isSuccess, isSuccessP, userData, connected } = useSelector(
-    (state) => state.covalent
-  );
-  const dispatch = useDispatch();
+	const {
+		nfts,
+		nftsPOLY,
+		userDataPOLY,
+		isLoadingP,
+		address,
+		isLoading,
+		isSuccess,
+		isSuccessP,
+		userData,
+		connected,
+	} = useSelector((state) => state.covalent);
+	const dispatch = useDispatch();
 
 	useEffect(() => {
-		if (address !== null ) {
+		if (address !== null) {
 			dispatch(getNFTsETH(address));
 			dispatch(getNFTsPOLY(address));
 		}
@@ -32,44 +40,46 @@ function Home() {
 		if (isSuccess && userData !== null) {
 			dispatch(getNfts());
 		}
-		if(isSuccessP && userDataPOLY !== null){
+		if (isSuccessP && userDataPOLY !== null) {
 			dispatch(getNftsPOLY());
 		}
+	}, [address, isSuccess, dispatch]);
 
-	}, [ address, isSuccess, dispatch]);
+	if (isLoading) {
+		return <Spinner />;
+	}
 
-  if (isLoading) {
-    return <Spinner />;
-  }
+	return (
+		<div className="w-full">
+			{connected === 'unsuccessful' && <Metamask />}
+			{connected === 'pending' && (
+				<div className="bg-blue w-full pt-10">
+					<h1>Please connect to MetaMask</h1>
+				</div>
+			)}
+			{(nfts !== null || nftsPOLY !== null) && <NavigationButtons />}
 
-  return (
-    <>
-     
-      {connected === 'unsuccessful' && <Metamask />}
-			{connected === 'pending' && <h1>Please connect to MetaMask</h1>}
-			{(nfts !== null || nftsPOLY !== null ) && <NavigationButtons />}
-      <div className="flex flex-col">
-		  <div className="flex flex-row">
-		  <div className="flex justify-around items-center w-full">
-        <Intropage
-          text={"Look at your NFT's"}
-          icon={<FaBinoculars className="text-5xl md:text-5xl" />}
-        />
-        <Intropage
-          text={"View Historical Pricing Info"}
-          icon={<FaChartLine className="text-5xl md:text-5xl" />}
-        />
-        <Intropage
-          text={"View Multiple EVM Compatible Chains"}
-          icon={<FaLinode className="text-5xl md:text-5xl" />}
-        />
+			<div className="flex flex-col ">
+				<div className="flex flex-row">
+					<div className="flex justify-around items-center  w-full">
+						<Intropage
+							text={"Look at your NFT's"}
+							icon={<FaBinoculars className="text-5xl md:text-5xl" />}
+						/>
+						<Intropage
+							text={'View Historical Pricing Info'}
+							icon={<FaChartLine className="text-5xl md:text-5xl" />}
+						/>
+						<Intropage
+							text={'View Multiple EVM Compatible Chains'}
+							icon={<FaLinode className="text-5xl md:text-5xl" />}
+						/>
+					</div>
+				</div>
+				<TechStack />
+			</div>
 		</div>
-		
-	</div>
-	<TechStack />
-	</div>
-</>
-  );
+	);
 }
 
 export default Home;

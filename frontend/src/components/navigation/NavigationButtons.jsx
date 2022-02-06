364e92a00f99825e25a0eac5f9cdce5d0c18d092
Howl1935 <travisphawley@gmail.com>
2022-02-06 14:00:48 -0800

    travis updates

--- frontend/src/components/navigation/NavigationButtons.jsx
+++ frontend/src/components/navigation/NavigationButtons.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function NavigationButtons() {
-
 	return (
-		<div>
-			<Link to='/gallery'> VIEW GALLERY </Link>
-		</div>
+		<>
+			<div className="flex justify-center items-center mt-10">
+                <div className='flex justify-center font-poppins text-xl text-[#000] items-center bg-[#FFC702] w-56 h-20 rounded-xl p-5 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:bg-[#FFFFD3] hover:translate-y-1'>
+				<Link to="/gallery" className='p-5'> VIEW GALLERY </Link>
+                </div>
+			</div>
+		</>
 	);
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       fontFamily: {
 
         roboto: "'Roboto', 'sans-serif'",
+        poppins: "'Poppins'"
 
       },
       colors: {
